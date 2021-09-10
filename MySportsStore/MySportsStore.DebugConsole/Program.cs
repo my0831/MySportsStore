4708f7a0f0d575f620385142a2851af020d322c3
my <my>
2021-09-09 21:05:16 -0400

    1[CHG] using EF;

--- MySportsStore/MySportsStore.DebugConsole/Program.cs
+++ MySportsStore/MySportsStore.DebugConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySportsStore.Domain.Concrete;
+using MySportsStore.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,29 @@ namespace MySportsStore.DebugConsole
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Start...");
+
+            using (var ctx = new EFDbContext())
+            {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    var product = new Product
+                    {
+                        Name = $"Football{i}",
+                        Price = i + 12,
+                        Description = $"This is a fooball{i}"
+                    };
+                    ctx.Products.Add(product);
+                }
+
+
+                ctx.SaveChanges();
+            }
+
+            Console.WriteLine("End");
+            Console.ReadLine();
         }
     }
 }
---
+++ MySportsStore/MySportsStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MySportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySportsStore.Domain.Concrete
+{
+    public class EFDbContext: DbContext
+    {
+        public DbSet<Product> Products { get; set;}
+    }
+}
