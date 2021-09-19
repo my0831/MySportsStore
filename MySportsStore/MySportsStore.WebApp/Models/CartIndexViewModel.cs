1e963d3239b98acb2568e96fcca2552b0ce24987
my <my>
2021-09-18 21:32:25 -0400

    1[CHG] add shopping cart

---
+++ MySportsStore/MySportsStore.WebApp/Models/CartIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MySportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MySportsStore.WebApp.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
